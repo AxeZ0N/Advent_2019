5149f39bb33203efa061f92ed958a1c2f388878d
AxeZ0N <reisbigryan@gmail.com>
2020-06-14 13:52:27 -0400

    Initial opcode computer with bugs

--- src/Advent_2_1.java
+++ src/Advent_2_1.java
@@ -1,5 +1,62 @@
+import java.util.*;
+
 public class Advent_2_1 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<String> input = (ArrayList<String>) Arrays.asList( new Scanner("input.txt").nextLine().split(",") );
+
+        int currOpcode = Integer.parseInt( input.get(0) );
+        int i = 0;
+
+
+        while (true){
+            switch (currOpcode){
+                case 1: {
+                    int pos1 = Integer.parseInt( input.get(currOpcode + 1) );
+                    int pos2 = Integer.parseInt( input.get(currOpcode + 2) );
+                    int pos3 = Integer.parseInt( input.get(currOpcode + 3) );
+
+                    String tempSum = String.valueOf( pos1 + pos2 );
+
+                    input.set( pos3, tempSum );
+
+                    break;
+                }
+
+                case 2: {
+                    int pos1 = Integer.parseInt( input.get(currOpcode + 1) );
+                    int pos2 = Integer.parseInt( input.get(currOpcode + 2) );
+                    int pos3 = Integer.parseInt( input.get(currOpcode + 3) );
+
+                    String tempSum = String.valueOf( pos1 * pos2 );
+
+                    input.set( pos3, tempSum );
+
+                    break;
+                }
+
+                case 99: {
+
+                    System.out.println(currOpcode);
+                    System.exit(0);
+
+                    int pos1 = Integer.parseInt( input.get(currOpcode + 1) );
+                    int pos2 = Integer.parseInt( input.get(currOpcode + 2) );
+                    int pos3 = Integer.parseInt( input.get(currOpcode + 3) );
+
+                    String tempSum = String.valueOf( pos1 + pos2 );
+
+                    input.set( pos3, tempSum );
+
+                    break;
+                }
+            }
+
+            i++;
+            currOpcode = Integer.parseInt( input.get(i) );
+        }
+
+
     }
+
+
 }
